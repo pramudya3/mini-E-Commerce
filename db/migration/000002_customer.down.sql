6a59c9367dcac7fd48e82abdaa6afceaa3b78532
nanda <pramudya500@gmail.com>
2022-11-07 13:01:06 +0700

    update db/migration table users;

--- db/migration/000002_customer.down.sql
+++ db/migration/000002_customer.down.sql
@@ -1,1 +1,1 @@
-drop table user;
+drop table users;
--- db/migration/000002_customer.up.sql
+++ db/migration/000002_customer.up.sql
@@ -1,8 +1,8 @@
-CREATE TABLE user(
+CREATE TABLE users(
  id serial primary key auto_increment,
  username varchar(30) not null unique,
  email varchar(50) not null unique,
- password varchar(30) not null,
+ password varchar(100) not null,
  gender varchar(6) not null,
  age int not null,
  address varchar(50) not null,
